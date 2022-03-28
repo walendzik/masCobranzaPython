5f60ab010e42f777e7e2436fc1afa0daadb9ae90
walendzik <ywalend@gmail.com>
2022-03-28 01:51:18 -0300

    nuevo commit

--- prueba_tecnica.py
+++ prueba_tecnica.py
@@ -3,18 +3,22 @@
 # Utilizar la librería Pandas para la resolución de los siguientes items:
 
 # Importación de la Librería:
-import pandas as pd 
+#from nbformat import write
+import pandas as pd
+
 
 #Código
 
 #----------------------------------------#
 #1 - Lectura del archivo.
 df= pd.read_excel(r'base_de_datos.xlsx')
+print(df)
 #Código
 
 #----------------------------------------#
 #2 - Mostrar el nombre de las columnas que contiene la base de datos.
-df.columns.values
+cols=list(df.columns)
+cols
 #Código
 
 
@@ -38,14 +42,24 @@ df.columns.values
 
 #----------------------------------------#
 #6 - Generar una nueva columna con el nombre de "Nombre Completo" donde sea una concatenación 
-# de las columnas "Apellido" y "Nombre". Esta nueva columna debe ser la 2da columna 
+# de las columnas "Apellido" y "Nombre". Esta nueva columna debe ser la 2da columna
 # de la base de datos, eliminando las 2 columnas mencionadas.
 
+df["Nombre completo"]= df ["Apellido"]+ " "+ df [ "Nombre"]
+
+df.drop(columns=["Apellido",'Nombre'])
+cols=list(df.columns)
+cols
+
+df[['Matrícula','Nombre completo', 'Categoría', 'Departamento', 'Sección', 'Salario', 'Fecha ingreso', 'Fch nacimiento'
+ ]]
+ 
 #Código
 
 #----------------------------------------#
 #7 - La columna H e I de la base de datos contiene los nombres de "Fch ingreso" y "Fch nacimiento". 
 # Renombrar estas columnas por su nombre correcto: "Fecha Ingreso" y "Fecha Nacimiento".
+df.rename({"Fecha ingreso": "Fecha Ingreso","Fch nacimiento": "Fecha Nacimiento"}, axis='columns' )
 
 #Código
 
@@ -58,12 +72,15 @@ df.columns.values
 #----------------------------------------#
 #9 - Generar una nueva columna a la derecha del resto, llamada "Actividad" donde todos sus valores 
 # sean "Activo".
-
+df['Actividad'] = "Activo" 
+print (df)
 #Código
 
 #----------------------------------------#
 #10 - Generar un nuevo excel con las modificaciones realizadas en los puntos anteriores.
 
+base_de_datos = pd.read_excel("formulario.xlsx")
+base_de_datos.to_excel ("formulario_exp.xlsx")
 #Código
 
 #----------------------------------------#
